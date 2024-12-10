eae2de1326d93ea41b979da707c340388dc51b63
Adeyeye Niyi Festus <adeyeyeniyi2@gmail.com>
2024-12-10 15:21:34 +0100

    Move add to cart function to cart.js file

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,2 +1,42 @@
 //create carts
 export const cart = [];
+
+// FUNCTION TO ADD ITEM TO CART LISTING  METHOD 1
+/* function addToCartList(productId) {
+  // START OF OBJECT
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    // NOTE THAT ITEM STANDS FOR INDIVIDUAL OBJECT TO BE PUSHED IN THE CART ARRAY
+    if (cartItem.productId === productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  // IF THERE IS A MATCHING OBJECT WHICH HAVE SAME PRODUCTNAME;
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    // ELSE IF THERE IS NO MATCHING OBJECT WITH SAME PRODUCT NAME IN THE CART ARRAY
+    cart.push({
+      productId, // PRODUCTID: PRODUCTID,
+      quantity: 1,
+    });
+  }
+  // END OF OBJECT
+} */
+
+export function addToCartList(productId) {
+  // Use find to search for a matching item
+  let matchingItem = cart.find((cartItem) => cartItem.productId === productId);
+
+  // If a matching item is found, increase its quantity
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    // Otherwise, add a new item to the cart
+    cart.push({
+      productId,
+      quantity: 1,
+    });
+  }
+}
--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,6 +1,6 @@
-// arrays of product objects - collpased and using products.js' products array instead
+// ARRAYS OF PRODUCT OBJECTS - COLLAPSED AND USING PRODUCTS.JS' PRODUCTS ARRAY INSTEAD
 /* const products = [
-  //1st start
+  //1ST START
   {
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -10,8 +10,8 @@
     },
     priceCents: 1290,
   },
-  //1st end
-  //2nd start
+  //1ST END
+  //2ND START
   {
     image: "images/products/intermediate-composite-basketball.jpg",
     name: "Intermediate Size Basketball",
@@ -21,8 +21,8 @@
     },
     priceCents: 2095,
   },
-  //2nd end
-  //3rd start
+  //2ND END
+  //3RD START
   {
     image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name: "Adults Plain Cotton T-Shirt - 2 Pack",
@@ -32,8 +32,8 @@
     },
     priceCents: 795,
   },
-  //3rd end
-  //4th start
+  //3RD END
+  //4TH START
   {
     image: "images/products/black-2-slot-toaster.jpg",
     name: "2 Slot Toaster - Black",
@@ -43,8 +43,8 @@
     },
     priceCents: 1795,
   },
-  //4th end
-  //5th start
+  //4TH END
+  //5TH START
   {
     image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "6 Piece White Dinner Plate",
@@ -54,8 +54,8 @@
     },
     priceCents: 2166,
   },
-  //5th end
-  //6th start
+  //5TH END
+  //6TH START
   {
     image: "images/products/6-piece-non-stick-baking-set.webp",
     name: "6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set",
@@ -65,8 +65,8 @@
     },
     priceCents: 3496,
   },
-  //6th end
-  //7th start
+  //6TH END
+  //7TH START
   {
     image: "images/products/plain-hooded-fleece-sweatshirt-yellow.jpg",
     name: "Plain Hooded Fleece Sweatshirt",
@@ -76,8 +76,8 @@
     },
     priceCents: 2500,
   },
-  //7th end
-  //8th start
+  //7TH END
+  //8TH START
   {
     image: "images/products/liquid-laundry-detergent-plain.jpg",
     name: "Liquid Laundry Detergent, 110 Loads, 82.5 Fl Oz",
@@ -87,8 +87,8 @@
     },
     priceCents: 2988,
   },
-  //8th end
-  //9th start
+  //8TH END
+  //9TH START
   {
     image: "images/products/luxury-tower-set-6-piece.jpg",
     name: "Luxury Towel Set - Graphite Gray",
@@ -98,8 +98,8 @@
     },
     priceCents: 3705,
   },
-  //9th end
-  //10th start
+  //9TH END
+  //10TH START
   {
     image: "images/products/women-chiffon-beachwear-coverup-black.jpg",
     name: "Women's Chiffon Beachwear Cover Up - Black",
@@ -109,8 +109,8 @@
     },
     priceCents: 1905,
   },
-  //10th end
-  //11th start
+  //10TH END
+  //11TH START
   {
     image: "images/products/knit-athletic-sneakers-gray.jpg",
     name: "Waterproof Knit Athletic Sneakers - Gray",
@@ -120,8 +120,8 @@
     },
     priceCents: 1905,
   },
-  //11th end
-  //12th start
+  //11TH END
+  //12TH START
   {
     image: "images/products/knit-athletic-sneakers-gray.jpg",
     name: "Waterproof Knit Athletic Sneakers - Gray",
@@ -131,18 +131,24 @@
     },
     priceCents: 1905,
   },
-  //12th end
+  //12TH END
 ]; */
 
 // IMPORT CART VARIABLE
 import { products } from "../data/products.js";
-import { cart } from "../data/cart.js";
-// import { cart as mycart } from "../data/cart.js";
+import { cart, addToCartList } from "../data/cart.js";
+// import { cart as myCart } from "../data/cart.js";
+//important all in a file as a object
+/* import * as cartObject from "../data/cart.js";
+//accessing as object syntex
+cartObject.cart;
+cartObject.addToCartList();
+ */
 
 let productsHTML = "";
-//looping through the products
+// LOOPING THROUGH THE PRODUCTS
 products.forEach((product) => {
-  //creation of html start
+  // CREATION OF HTML START
   const html = `<div class="product-container">
           <div class="product-image-container">
             <img
@@ -195,68 +201,60 @@ products.forEach((product) => {
             product.id
           }">Add to Cart</button>
         </div>`;
-  //creation of html end
+  // CREATION OF HTML END
   productsHTML += html;
 });
 
 console.log(productsHTML);
-// placing the productsHTML on the webpage
+// PLACING THE PRODUCTSHTML ON THE WEBPAGE
 const productsGridDisplay = document.querySelector(".js-products-grid");
 productsGridDisplay.innerHTML = productsHTML;
 
-//getting the  js-add-to-cart btn
+//FUNCTION TO DISPLAY CART WITH DOM : METHOD 2
+let cartQuantity;
+function displayCartQuantity() {
+  // DISPLAYING THE CART QUANTITY IN THE CART ICON AT THE TOP RIGHT OF THE PAGE
+  cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  // DISPLAY THE CART QUANTITY ON THE PAGE USING DOM
+  const cartQuantityDisplay = document.querySelector(".js-cart-quantity");
+
+  cartQuantityDisplay.innerHTML = `${cartQuantity}`;
+}
+
+// GETTING THE  JS-ADD-TO-CART BTN
 const addToCartBtn = document.querySelectorAll(".js-add-to-cart-button");
 
-//start of addToCartbtn.forEach()
-//loop through all js-add-to-cart btns
+// START OF ADDTOCARTBTN.FOREACH()
+// LOOP THROUGH ALL JS-ADD-TO-CART BTNS
 addToCartBtn.forEach((btn) => {
-  //start of btn.addEventListner function
-  //add event listner to all add-to-cart-btns
+  // START OF BTN.ADDEVENTLISTNER FUNCTION
+  // ADD EVENT LISTNER TO ALL ADD-TO-CART-BTNS
   btn.addEventListener("click", () => {
-    //getting the data- attributes attached to the individual btn
-    //gives a DOMStringMap which is object-like
-    //then getting the data-product-name attribute using camel case (productName) from initial kebab case (data-product-name)
+    // GETTING THE DATA- ATTRIBUTES ATTACHED TO THE INDIVIDUAL BTN
+    // GIVES A DOMSTRINGMAP WHICH IS OBJECT-LIKE
+    // THEN GETTING THE DATA-PRODUCT-NAME ATTRIBUTE USING CAMEL CASE (PRODUCTNAME) FROM INITIAL KEBAB CASE (DATA-PRODUCT-NAME)
     const productId = btn.dataset.productId;
 
-    //push in cart array as an object
-    // & also checking if we already hve a matching productName in the cart so the quantity always increase by 1 bfr pushing in the cart
-    //start of object
-    let matchingItem;
-    cart.forEach((item) => {
-      //note that item stands for individual object to be pushed in the cart array
-      if (item.productId === productId) {
-        matchingItem = item;
-      }
-    });
-    // if there is a matching object which have same productName;
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      //else if there is no matching object with same product name in the cart array
-      cart.push({
-        productId, //productId: productId,
-        quantity: 1,
-      });
-    }
-    //end of object
+    // PUSH IN CART ARRAY AS AN OBJECT
+    // & ALSO CHECKING IF WE ALREADY HVE A MATCHING PRODUCTNAME IN THE CART SO THE QUANTITY ALWAYS INCREASE BY 1 BFR PUSHING IN THE CART
 
-    // displaying the cart quantity in the cart icon at the top right of the page
-    let cartQuantity = 0;
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
+    // INVOKE THE FUNCTION
+    addToCartList(productId);
 
-    //display the cart quantity on the page using DOM
-    const cartQuantityDisplay = document.querySelector(".js-cart-quantity");
+    // INVOKE FUNCTION
+    displayCartQuantity();
 
-    cartQuantityDisplay.innerHTML = `${cartQuantity}`;
-
+    // LOG IN THE CONSOLE
     console.log(cart);
     console.log(cartQuantity);
   });
-  //end of btn.addEventListner function
+  // END OF BTN.ADDEVENTLISTNER FUNCTION
 });
-//start of addToCartbtn.forEach()
+// START OF ADDTOCARTBTN.FOREACH()
 
-//function for add to cart btn
-function addToCart() {}
+// FUNCTION FOR ADD TO CART BTN
+// FUNCTION ADDTOCART() {}
