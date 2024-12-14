686bd575b1485f6ab414ad2d46fa3d82aa86db67
Adeyeye Niyi Festus <adeyeyeniyi2@gmail.com>
2024-12-14 10:42:19 +0100

    did little tweaks in naming variables generally

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,7 @@
 import { renderCartItems } from "./checkout/orderSummary.js";
 
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+
 // INVOKE THE FUNCTION TO RENDER THE CART ON PAGE LOAD
 renderCartItems();
+renderPaymentSummary();
